e446c1adca59d78bd20f82ce9a6149c43b5530fb
Matt DeClaire <matt@weebly.com>
2012-11-26 17:56:51 -0800

    app id publish input, handle ios urls

--- iposts.js
+++ iposts.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
 	var options = $('#iposts-publish-options').hide();
 	$("[name=iposts\\[app_id\\]]").keyup(function() {
+		this.value = this.value.replace(/https?:\/\/.*.apple.com\/.*id([0-9]+).*/, "$1");
 		this.value = this.value.replace(/[^0-9]/g, '');
 		if (this.value.length) {
 			options.slideDown();
